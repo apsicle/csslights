da0c011635d3ca1484eae76ddc0a376c23d51b8c
apsicle <rcyan@email.wm.edu>
2016-11-29 16:17:54 -0500

    working lights and rope

--- hanglights.js
+++ hanglights.js
@@ -4,29 +4,6 @@
 */
 var diver = document.getElementById("overlay");
 
-for (var i = 0; i < 10; i++) {
-	for (var j = 0; j < 20; j++) {
-		var mylight = document.createElement("div");
-		mylight.className = "light";
-		var toppart = document.createElement("div");
-		toppart.className = "square";
-		mylight.appendChild(toppart);
-		var bottompart = document.createElement("div");
-		bottompart.className = "oval";
-		mylight.appendChild(bottompart);
-		diver.appendChild(mylight);
-	}
-}
-
-
-/*
-var div = document.body.getElementsByClassName("light");
-var div = div[1];
-var child = div.childNodes;
-child[1].style.background = "yellow";
-*/
-
-var divs = document.body.getElementsByClassName("light");
 var colors = ["red", "blue", "yellow", "green", "orange"];
 var num_colors = colors.length;
 var prev_color = "red";
@@ -40,15 +17,47 @@ function pick_color(previous) {
 	
 	return colors[selected];
 }
-	
-for (var i = 0; i < divs.length; i++) {
-	var div = divs[i];
-	var bulb = div.childNodes[1];
-	var new_color = pick_color(prev_color);
-	bulb.style.background = new_color;
-	prev_color = new_color;
+
+
+/*Loop creates a 10x20 grid of light divs, which each contain a top (square) and bottom (oval). The ovals, which are the lights,
+get assigned a random color with the constraint that two of the same colors cannot be touching. Then, the box shadow is applied, and
+the whole light is appended to the div overlaid on the canvas. */
+
+
+
+for (var i = 0; i < 10; i++) {
+	for (var j = 0; j < 20; j++) {
+		var mylight = document.createElement("div");
+		mylight.className = "light";
+		mylight.style.height = "10vh";
+		var toppart = document.createElement("div");
+		toppart.className = "square";
+		mylight.appendChild(toppart);
+		var bottompart = document.createElement("div");
+		bottompart.className = "oval";
+		var new_color = pick_color(prev_color);
+		bottompart.style.backgroundColor = new_color;
+		bottompart.style.boxShadow = "0px 7px 7px -7px " + new_color;
+		prev_color = new_color;
+		mylight.appendChild(bottompart);
+		diver.appendChild(mylight);
+		
+
+		/*
+		ctx.bezierCurveTo(10,1,40,1,mylocation.top, mylocation.left);
+		ctx.stroke();*/
+	}
 }
 
+
+/*
+var div = document.body.getElementsByClassName("light");
+var div = div[1];
+var child = div.childNodes;
+child[1].style.background = "yellow";
+*/
+
+var divs = document.body.getElementsByClassName("light");
 var light_matrix = [];
 
 for (var i = 0; i < 10; i++) {
@@ -59,34 +68,76 @@ for (var i = 0; i < 10; i++) {
 	light_matrix.push(row);
 }
 
+var c=document.createElement("canvas");
+c.width = window.innerWidth;
+c.height = window.innerHeight;
+document.getElementById("container").appendChild(c);	
+var ctx=c.getContext("2d");
+ctx.beginPath();
+
+ctx.strokeStyle = "grey";
+var oldLocationLeft = 0;
+var oldLocationTop = 0;
+
+for (var i = 0; i < 10; i++) {
 	
+	for (var j = 0; j < 20; j++) {
+		
+		var myLocation = $(light_matrix[i][j].childNodes[0]).offset()
+		var ctr1Left = ((myLocation.left - oldLocationLeft) / 3) + oldLocationLeft;
+		var ctr2Left = 2 * ((myLocation.left - oldLocationLeft) / 3) + oldLocationLeft;
+		
+		ctx.bezierCurveTo(ctr1Left, myLocation.top + 8, ctr2Left, myLocation.top + 8, myLocation.left, myLocation.top);
+		
+		ctx.stroke();
+		oldLocationLeft = myLocation.left;
+		oldLocationTop = myLocation.top;
+		ctx.beginPath();
+		ctx.moveTo(myLocation.left, myLocation.top);
+		
+		
+	}
+}
+
+
 function lights_on(i, j) {
 	var bulb = light_matrix[i][j].childNodes[1];
-	bulb.style.boxShadow = "0px 7px 20px 10px #ffef00";
-	bulb.style.opacity = 1;
+	var shadowColor = bulb.style.backgroundColor;
+	window.setTimeout(function() {bulb.style.boxShadow = "0px 7px 20px 3px " + shadowColor; bulb.style.opacity = 1;}, 1500);
+	window.setTimeout(lights_off, 2000, i, j);
 }
 
 function lights_off(i, j) {
 	var bulb = light_matrix[i][j].childNodes[1];
-	bulb.style.boxShadow = "0px 7px 7px 0px #ffef00";
-	bulb.style.opacity = 0.5;
+	var shadowColor = bulb.style.backgroundColor;
+	window.setTimeout(function() {bulb.style.boxShadow = "0px 7px 7px -7px " + shadowColor; bulb.style.opacity = 0.75;}, 1500);
 }
 
 var bulb = light_matrix[1][1].childNodes[1];
+var order = [];
+for (var i = 0; i < 100; i++) {
+	order.push([Math.floor(Math.random() * 10), Math.floor(Math.random() * 20)]);
+}
 
-bulb.style.background = "red";
-window.setTimeout(lights_on, 1000, 1, 1);
-window.setTimeout(lights_off, 2000, 1, 1);
 
+var lighter = 0;
+function light_it_up() {
+	
+	lights_on(order[lighter][0],order[lighter][1]);
+	
+	lights_on(Math.floor(Math.random() * 10), Math.floor(Math.random() * 20));
+	if (++lighter < 100) {
+		if (lighter % 9 === 0) {
+			window.setTimeout(light_it_up, 3000);
+    }
+    else {
+			window.setTimeout(light_it_up, 1500);
+    }
+	}
+}
 
+light_it_up();
 
-var c=document.getElementById("mycanvas");
-var ctx=c.getContext("2d");
-ctx.beginPath();
-ctx.moveTo(1,1);
-ctx.bezierCurveTo(10,1,40,1,51,1);
-ctx.strokeStyle = "grey";
-ctx.stroke();
 
 
 	
